76e5935f18ee804c6f7467001b78da55ee853725
shyyko.serhiy@gmail.com <shyyko.serhiy@gmail.com>
2015-02-10 19:56:52 -0800

    changed look and feel

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,27 @@
-console.log('This would be the main JS file.');
+window.addEventListener('load', function () {
+  var rulezH = new Rulez({
+    element: document.getElementById('svgH'),
+    layout: 'horizontal'
+  });
+  rulezH.render();
+  var rulezV = new Rulez({
+    element: document.getElementById('svgV'),
+    layout: 'vertical',
+    texts: [
+      {
+        pixelGap: 100,
+        rotation: 90,
+        offset: 20,
+        className: 'rulez-text',
+        type: 'text'
+      }
+    ]
+  });
+  rulezV.render();
+  var scroll = window;
+  scroll.addEventListener('scroll', function () {
+    rulezH.scrollTo(scroll.scrollX);
+    rulezV.scrollTo(scroll.scrollY);
+  });
+
+});
