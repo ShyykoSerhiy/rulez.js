5e12f79b697a53d88071dbbe25a8f0222ad1c2bd
shyyko.serhiy@gmail.com <shyyko.serhiy@gmail.com>
2016-08-21 15:36:11 +0300

    added typescript definition

---
+++ src/typings/rulez.d.ts
@@ -0,0 +1,47 @@
+declare module 'rulez.js' {
+	export interface DivisionConfig {
+		strokeWidth?: number,
+		type?: 'rect'|'line',
+		className?: string,
+		pixelGap?: number,
+		lineLength?: number,
+		renderer?: (el:Element)=>{}
+	}
+	export interface TextConfig {
+		rotation?: number,
+		offset?: number,
+		className?: string,
+		/**
+		 * Wherever to show or not to show units alongside text
+		 */
+		showUnits: boolean,
+		centerText?: {
+			by: 'width'|'height',
+			operation: 'sub'|'sum'
+		},
+		renderer: (el:Element)=>{}
+	}
+	export interface RulezConfig {
+		width?: number,
+		height?: number,
+		element: Element|any,
+		layout?: 'horizontal'| 'vertical',
+		alignment?: 'top'|'left'|'right'|'bottom',
+		units?: 'em'|'ex'|'px'|'pt'|'pc'|'cm'|'mm'|'in'|'',
+		divisionDefaults?: DivisionConfig,
+		textDefaults?: TextConfig,
+		divisions?: DivisionConfig[],
+		texts?: TextConfig[]
+	}
+	class Rulez {
+		constructor(config:RulezConfig);
+
+		getUnitConversionRate:() => number;
+		render:() => void;
+		resize:() => void;
+		saveAsImage:(saveFinishCallback:(imageDataUrl:string)=> void) => void;
+		scrollTo:(pos:number, useUnits:boolean) => void;
+		setScale:(scaleValue:number) => void;
+	}
+	export default Rulez;
+}
