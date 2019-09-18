0e4133a6c848dea87799c741f944365d9c4100fc
shyyko.serhiy <shyyko.serhiy@gmail.com>
2019-09-18 11:17:05 -0400

    feat: added snap to grid for guides

--- src/js/rulez.js
+++ src/js/rulez.js
@@ -412,24 +412,16 @@
                 guideConfig.position = startGuidePos - (diff * scale);   
                 if (e.shiftKey) {
                     // snap to the grid
-                    console.log('ZZZZZZZ')
-                    console.log('position', guideConfig.position)
                     var halfSnap = c.guideSnapInterval / 2;
-                    console.log('halfSnap', halfSnap)
                     var mod = guideConfig.position % c.guideSnapInterval;
-                    console.log('mod', mod)
                     var absMod = Math.abs(mod);
-                    console.log('absMod', absMod)
                     var diffSnap = mod;
                     if (absMod > halfSnap) {
-                        console.log('bigger')
                         diffSnap = Math.sign(mod) * (c.guideSnapInterval - absMod);
-                        console.log('diffSnap')
                         guideConfig.position = Math.round(guideConfig.position + diffSnap);
                     } else {
                         guideConfig.position = Math.round(guideConfig.position - diffSnap);
                     }
-                    console.log('newPosition', guideConfig.position);
                 } else {
                     guideConfig.position = Math.round(guideConfig.position);
                 }
